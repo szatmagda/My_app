1849b63e835594bcdfcb5ad16b12a6ddc6a56a54
szatmagda <szatmagda@gmail.com>
2025-05-28 01:17:13 +0200

    inne

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var number = 0  //liczba startowa
+
+        val textField = findViewById<TextView>(R.id.textField)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button1.setOnClickListener{
+            number++ //zwiększenie liczby o jeden
+            textField.text = "$number" //zmiana na stringa i przypisujemy do pola text, który znajduje się w textField
+        }
+
+        button2.setOnClickListener{
+            number--
+            textField.text = "$number"
         }
     }
 }
